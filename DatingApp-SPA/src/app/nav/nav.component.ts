2561b092b98f96fb110ed440e4d354c102562b6c
Payal <payal_dobaria@hotmail.com>
2020-10-15 12:49:49 +0100

    End of Section 4

---
+++ DatingApp-SPA/src/app/nav/nav.component.ts
@@ -0,0 +1,40 @@
+import { Component, OnInit, ɵclearResolutionOfComponentResourcesQueue } from '@angular/core';
+
+import { AuthService } from '../_services/auth.service';
+
+@Component({
+  selector: 'app-nav',
+  templateUrl: './nav.component.html',
+  styleUrls: ['./nav.component.scss']
+})
+export class NavComponent implements OnInit {
+
+  model: any = {};
+
+  constructor(private authService: AuthService) { }
+
+  ngOnInit(): void {
+  }
+
+  // tslint:disable-next-line: typedef
+  login() {
+     this.authService.login(this.model).subscribe(next => {
+      console.log('Logged in successfully');
+    // tslint:disable-next-line: no-shadowed-variable
+    }, error => {
+      console.log('Failed to login');
+    });
+  }
+
+  // tslint:disable-next-line: typedef
+  loggedIn(){
+    const token = localStorage.getItem('token');
+    return !!token;
+  }
+
+  // tslint:disable-next-line: typedef
+  logout(){
+    localStorage.removeItem('token');
+    console.log('Logged out');
+  }
+}
